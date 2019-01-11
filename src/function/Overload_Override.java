c95526a625f4b17ca3baef49edd6733a3d388a04
mygbo <mygbo@DESKTOP-HNBB94V>
2019-01-11 09:39:26 +0800

    重载&重写

---
+++ src/function/Overload_Override.java
@@ -0,0 +1,29 @@
+package function;
+
+public class Overload_Override {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+	
+	public static int myadd(int num1,int num2) {
+		return num1+num2;
+	}
+	public static int myadd(short num1,short num2) {
+		return num1+num2;
+	}
+	public static int myadd(long num1,long num2) {
+		return num1+num2;
+	}
+	public static int myadd(byte num1,byte num2) {
+		return num1+num2;
+	}
+	public static int myadd(double num1,double num2) {
+		return num1+num2;
+	}
+	public static int myadd(num1,num2) {
+		return num1+num2;
+	}
+
+}
