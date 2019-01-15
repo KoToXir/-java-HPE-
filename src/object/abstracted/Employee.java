81c117cee5706234447bf629d38f80a93451bd19
mygbo <mygbo@DESKTOP-HNBB94V>
2019-01-15 15:07:02 +0800

    抽象类、抽象方法

--- src/object/abstracted/Employee.java
+++ src/object/abstracted/Employee.java
@@ -1,7 +1,16 @@
-
+/*
+ * 
+ */
 public abstract class Employee {
 	private String name;
 	private String id;
 	private String salary;
 	
+	public Employee(String n,String i,String s) {
+		this.name = n;
+		this.id = i;
+		this.salary = s;
+	}
+	
+	public abstract void work();
 }
