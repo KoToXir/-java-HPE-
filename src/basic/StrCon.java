18d78b557b207dcc94f19e1be6164fbec19c8a35
mygbo <mygbo@DESKTOP-HNBB94V>
2019-01-16 11:34:56 +0800

    字符串拼接测试

---
+++ src/basic/StrCon.java
@@ -0,0 +1,15 @@
+/**
+ * 测试String.concat()方法拼接字符串
+ */
+package basic;
+
+public class StrCon {
+	
+	public static void main(String args[]) {
+		String a = "abc";
+		String b = "def";
+		String c = "ghi";
+		System.out.println(a.concat(b.concat(c)));
+		
+	}
+}
