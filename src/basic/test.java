07b7d06bdc117e72fdf3858ffdcb43944f70b711
mygbo <mygbo@DESKTOP-HNBB94V>
2019-02-22 15:13:57 +0800

    其他

---
+++ src/basic/test.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+int a;
+
+	public static void main(String[] args) {
+		List mList = new ArrayList();
+		
+	}
+
+}
---
+++ src/basic/test1.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int k=0;
+		int t=k++ + ++k + k + k++ ;
+		System.out.println(t);
+	}
+
+}
